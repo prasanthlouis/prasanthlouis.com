322d0c064abd05441ca0ffa5d39b91e8214e70e1
prasanthlouis <prasanthlouis21@gmail.com>
2015-07-13 11:50:04 +0530

    Initial Commit

---
+++ PHP FILES/index.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+
+	 </head>
+<body bgcolor="#E6E6FA">
+
+<nav class="navbar navbar-inverse">
+
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				 <span class="icon-bar"></span>
+				 <span class="icon-bar"></span>
+				 <span class="icon-bar"></span> 
+			</button>
+			<a class="navbar-brand" href="http://prasanthlouis.com">PRASANTH LOUIS</a>
+		</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li><a href="http://github.com/prasanthlouis">GITHUB</a></li>
+					<li><a href="http://www.hackerearth.com/users/prasanthlouis/">HACKEREARTH</a></li>
+					<li><a href="http://smphacks.blogspot.in/">MY BLOGS</a></li>
+					<li><a href="https://www.hackthissite.org/user/view/prasanth21/">HACKTHISSITE</a></li>
+					<li><a href="http://forum.xda-developers.com/member.php?u=6000984">XDA DEVELOPER</a></li>
+					<li><a href="http://www.codecademy.com/PrasanthL">CODE ACADEMY</a></li>
+					<li><a href="http://stackoverflow.com/users/3293761/prasanth-louis">STACKOVERFLOW</a></li>
+					<li><a href="index1.html">MAIL ME</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+<div class="part1">
+<section class="py2">
+<div data-fitter-happier-text class="">Hey I'm Prasanth Louis</div>
+<div data-fitter-happier-text class="">I'm a happy go lucky 21 year old</div>
+<div data-fitter-happier-text class="">I love to code all the time! Especially with</div>
+<div data-fitter-happier-text class=""> Python, C, C++</div>
+<div data-fitter-happier-text class="">Plus I'm not so bad at languages like</div>
+<div data-fitter-happier-text class="">Java, Html/CSS</div>
+<div data-fitter-happier-text class="">Coding Android apps are also fun</div>
+<div data-fitter-happier-text class="">When I'm not coding, I take my mind off things by </div>
+<div data-fitter-happier-text class="">playing the guitar,</div>
+<div data-fitter-happier-text class="">swimming, and playing basketball</div>
+<div data-fitter-happier-text class="">I also hold a black-belt in tae-kwon-do!</div>
+<div data-fitter-happier-text class="">(Yes I can kick butt)</div>
+<div data-fitter-happier-text class="">Let's strive to make everyone around us happy!</div>
+<div data-fitter-happier-text class="">"Grant me the serenity to accept the things I cannot change,</div>
+<div data-fitter-happier-text class="">the courage to change the things I can,</div> 
+<div data-fitter-happier-text class="">and the wisdom to know the difference."</div>
+<br/><br/>
+<p style="font-size:125%; text-align:center;"><a href="page1.html">MY RESUME AND WORK EXPERIENCE</a></p>
+<p style="font-size:125%; text-align:center;"><a href="photoindex.html">LIFE IN PHOTOS</a></p>
+<p style="font-size:125%; text-align:center;"><a href="index1.html">MAIL ME</a></p>
+
+<hr>
+<?php include 'count.php';
+?>
+
+
+<section>
+
+</div>
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="fitter-happier-text.js"></script>
+	<script type="text/javascript"></script>
+	<script>
+    var nodes = document.querySelectorAll('[data-fitter-happier-text]');
+    fitterHappierText(nodes, { baseline: 14,  paddingY: 6 });
+  </script>
+  </body>
+</html>
